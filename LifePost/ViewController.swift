85b0e6fcf2c52b84169b0cc269403ade04660538
Johhny <nikusor.94@gmail.com>
2019-01-11 14:54:38 +0200

    Initial Commit

---
+++ LifePost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifePost
+//
+//  Created by Ciobanasu Ion on 1/11/19.
+//  Copyright © 2019 Ciobanasu Ion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
